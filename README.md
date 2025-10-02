7a134450aec3491a91946b4fffe2819f426ac4f0
James <37276661+capjamesg@users.noreply.github.com>
2025-10-03 00:22:07 +0100

    add segmentation

--- README.md
+++ README.md
@@ -13,9 +13,11 @@
 
 This repository contains the code supporting the RF-DETR target model for use with [Autodistill](https://github.com/autodistill/autodistill).
 
-[RF-DETR](https://github.com/roboflow/rf-detr) is a real-time, transformer-based object detection model developed by Roboflow and released under the Apache 2.0 license.
+[RF-DETR](https://github.com/roboflow/rf-detr) is a real-time, transformer-based object detection and segmentation model developed by Roboflow and released under the Apache 2.0 license.
 
 RF-DETR-N outperforms YOLO11-N by 10 mAP points on the Microsoft COCO benchmark while running faster at inference. On RF100-VL, RF-DETR achieves state-of-the-art results, with RF-DETR-M beating YOLO11-M by an average of 5 mAP points across aerial datasets including drone, satellite, and radar.
+
+You can train both RF-DETR object detection and segmentation models with Autodistill.
 
 Read the full [Autodistill documentation](https://autodistill.github.io/autodistill/).
 
@@ -27,19 +29,37 @@ To use the RF-DETR target model, you will need to install the following dependen
 pip3 install autodistill-rfdetr
 ```
 
-## Quickstart
+## Getting Started
+
+To use this package, you will need a dataset in the Microsoft COCO Segmentation data format.
+
+## Quickstart (Object Detection)
 
 ```python
-from autodistill_rfdetr import RFDETR
+from autodistill_rfdetr import RFDETRBase
 
-target_model = RFDETR("base")
+target_model = RFDETRBase()
 
 # train a model
-target_model.train("./labeled_data/data.yaml", epochs=200)
+target_model.train("./labeled_data/", epochs=100)
 
 # run inference on the new model
 pred = target_model.predict("./labeled_data/train/images/dog-7.jpg", conf=0.01)
 ```
+
+## Quickstart (Instance Segmentation)
+
+```python
+from autodistill_rfdetr import RFDETRSegPreview
+
+target_model = RFDETRSegPreview()
+
+target_model.train("./labeled_data/data.yaml", epochs=100)
+
+# run inference on the new model
+pred = target_model.predict("./labeled_data/train/images/dog-7.jpg", conf=0.01)
+```
+
 
 ## License
 
--- autodistill_rfdetr/model.py
+++ autodistill_rfdetr/model.py
@@ -1,6 +1,9 @@
 from autodistill.detection import DetectionTargetModel
-from rfdetr import RFDETRBase as BaseModel
+from rfdetr import RFDETRNano as NanoModel
+from rfdetr import RFDETRSmall as SmallModel
+from rfdetr import RFDETRMedium as MediumModel
 from rfdetr import RFDETRLarge as LargeModel
+from rfdetr import RFDETRSegPreview as SegPreviewModel
 import supervision as sv
 
 class RFDETR(DetectionTargetModel):
@@ -11,26 +14,48 @@ class RFDETR(DetectionTargetModel):
         self.model.optimize_for_inference()
         return self.model.predict(input, threshold=confidence)
 
-    def train(self, dataset_yaml, epochs=25, output_dir="training-output"):
+    def train(self, dataset_yaml, epochs=25, output_dir="training-output", **kwargs):
         self.model.train(
             dataset_dir=dataset_yaml,
             epochs=epochs,
             batch_size=4,
             grad_accum_steps=4,
             lr=1e-4,
-            output_dir=output_dir
+            output_dir=output_dir,
+            **kwargs
         )
 
-class RFDETRBase(RFDETR):
+class RFDETRNano(RFDETR):
     def __init__(self, checkpoints = None):
         if checkpoints:
-            self.model = BaseModel(checkpoints=checkpoints)
+            self.model = NanoModel(checkpoints=checkpoints)
         else:
-            self.model = BaseModel()
-        
+            self.model = NanoModel()
+
+class RFDETRSmall(RFDETR):
+    def __init__(self, checkpoints = None):
+        if checkpoints:
+            self.model = SmallModel(checkpoints=checkpoints)
+        else:
+            self.model = SmallModel()
+
+class RFDETRMedium(RFDETR):
+    def __init__(self, checkpoints = None):
+        if checkpoints:
+            self.model = MediumModel(checkpoints=checkpoints)
+        else:
+            self.model = MediumModel()
+
 class RFDETRLarge(RFDETR):
     def __init__(self, checkpoints = None):
         if checkpoints:
             self.model = LargeModel(checkpoints=checkpoints)
         else:
             self.model = LargeModel()
+
+class RFDETRSegPreview(RFDETR):
+    def __init__(self, checkpoints = None):
+        if checkpoints:
+            self.model = SegPreviewModel(checkpoints=checkpoints)
+        else:
+            self.model = SegPreviewModel()
